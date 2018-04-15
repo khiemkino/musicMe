a2dc49e6675a870d9d5bb36c9b9af23d1fbf5ab5
hb <khiemkino@gmail.com>
2018-04-15 10:53:52 +0700

    Update

--- src/Utils/globalFunction.js
+++ src/Utils/globalFunction.js
@@ -1,15 +1,8 @@
-import { statusName } from '@/globalConstants'
-import { AppColor } from '@/globalStyles'
-import moment from 'moment'
-import I18n from 'react-native-i18n'
+import io from 'socket.io-client'
 
+var socket
 export const keyExtractor = (item, index) => {
   return index.toString()
-}
-
-export const subBeforeAfter = (string) => {
-  let strSplit = string.split(',')
-  return strSplit
 }
 
 export const getMinutes = (second) => {
@@ -19,55 +12,17 @@ export const getMinutes = (second) => {
   return minutes + ':' + (seconds.toFixed().toString().length === 1 ? '0' + seconds.toFixed() : seconds.toFixed())
 }
 
-export const getColorSignal = (status) => {
-  const nameColor = status === statusName.opening
-    ? AppColor.main
-    : status === statusName.failed ? AppColor.gray : AppColor.green
-  return nameColor
-}
-
-export const convertTimestampToDate = (strTimestamp) => {
-  const date = new Date(strTimestamp)
-  let strTime = moment(date).format('YYYY/MM/DD')
-  return strTime
-}
-
-export const convertTimestampToDateMonth = (strTimestamp) => {
-  const date = new Date(strTimestamp)
-  let strTime = moment(date).format('MMMM D')
-  return strTime
-}
-
-export const convertTimestampToDateTime = (strTimestamp) => {
-  const date = new Date(strTimestamp)
-  let strTime = moment(date).format('YYYY/MM/DD hh:mm')
-  return strTime
-}
-
-export const formatNumber = (strNumber, isFiatMoney) => {
-  let stringNumber = Number(strNumber).toFixed(8).replace(/\.?0+$/, '')
-  let dotsCount = countDots(stringNumber, '\\.')
-  let precision = null
-  if (dotsCount === 1) {
-    let decimalCount = stringNumber.length - stringNumber.indexOf('.') - 1
-    decimalCount = decimalCount <= 2 ? 2 : decimalCount
-    precision = decimalCount > 8 ? 8 : decimalCount
-  } else {
-    precision = 2
-  }
-  let value = I18n.toNumber(stringNumber,
-    { separator: '.', precision: isFiatMoney ? 2 : precision, delimiter: ',' })
-  return value
-}
-
-export const countDots = (strString, strLetter) => {
-  return (strString.match(RegExp(strLetter, 'g')) || []).length
-}
-
-export const showAlert = (THIS, strMessage, isError) => {
-  THIS.showAlert(strMessage, isError)
-}
-
-export const calculatePercentIncrease = (calculateNum, compareNum) => {
-  return formatNumber(((compareNum - calculateNum) / (calculateNum)) * 100, true) + '%'
-}
+// export const updateSongServer = (dispatch) => {
+//   // Create WebSocket connection.
+//   const ws = new WebSocket('wss://stream.binance.com:9443/ws/engbtc@aggTrade')
+//   // Connection opened
+//   ws.addEventListener('open', function (event) {
+//     console.log(event)
+//   })
+//   // Listen for messages
+//   ws.addEventListener('message', function (newEvent) {
+//     var wsEvent = JSON.parse(newEvent.data)
+//     console.log(wsEvent)
+//     dispatch(getExchangeRate(wsEvent))
+//   })
+// }
